f7418837475447e10d2b097cea485cb3a3e66df6
Caio F. Masumoto <caio.fornaciari@edu.unifil.br>
2022-11-27 22:29:53 -0300

    2.0

--- .github/binary_tree.py
+++ .github/binary_tree.py
@@ -1,63 +1,105 @@
 from node import Node
 
-class  BinaryTree:
-    def __init__(self):
-        self.root = None
 
+class BinaryTree:
+    def __init__(self):
+        self.valor = None
     def getRoot(self):
         return self.root
 
     def adicionar(self, val):
-        if self.root is None:
-            self.root = Node(val)
+        if self.esquerda is not None:
+            self.adicionar(val, self.esquerda)
         else:
-            self._adicionar(val, self.root)
+            self.esquerda = Node(val)
 
-    def _adicionar(self, val, node):
-        if val < node.valor:
-            if node.esquerda is not None:
-                self._adicionar(val, node.esquerda)
-            else:
-                node.esquerda = Node(val)
+        if self.direita is not None:
+                self.adicionar(val, self.direita)
         else:
-            if node.direita is not None:
-                self._adicionar(val, node.direita)
+             self.direita = Node(val)
+
+    def preOrder(self, começo, stepe):
+        if começo:
+            stepe += (str(começo.value) + "/")
+            stepe = self.preOrder(começo.esquerda, stepe)
+            stepe = self.preOrder(começo.direita, stepe)
+        return stepe
+
+    def inOrder(self, stepe, começo):
+        if começo:
+            stepe = self.inOrder(começo.esquerda, stepe)
+            stepe += (str(começo.value) + "/")
+            stepe = self.inOrder(começo.direita, stepe)
+        return stepe
+
+    def postOrder(self, stepe, começo):
+        if começo:
+            stepe = self.postOrder(começo.esquerda, stepe)
+            stepe = self.postOrder(começo.direita, stepe)
+            stepe += (str(começo.value) + "/")
+        return stepe
+
+    def remover(self, valor):
+        global filho_esquerda
+        if self.root == None:
+            return False
+        atual = self.root
+        pai = self.root
+        filho_esq = True
+
+        while atual.item != valor:
+            pai = atual
+            if valor < atual.item:
+                atual = atual.esquerda
+                filho_esquerda = True
             else:
-                node.direita = Node(val)
+                atual = atual.direita
+                filho_esquerda = False
+            if atual == None:
+                return False
 
+        if atual.esquerda == None and atual.direita == None:
+            if atual == self.root:
+                self.root = None
+            else:
+                if filho_esquerda:
+                    pai.esquerda = None
+                else:
+                    pai.direita = None
 
-    def preOrder_print(self, começo, string):
-        if começo:
-            string += (str(começo.value) + "/")
-            string = self.preOrder_print(começo.esquerda, string)
-            string = self.preOrder_print(começo.direita, string)
-        return string
+        elif atual.direita == None:
+            if atual == self.root:
+                self.root = atual.esquerda
+            else:
+                if filho_esquerda:
+                    pai.esquerda = atual.esquerda
+                else:
+                    pai.direita = atual.esquerda
 
-    def inOrder_print(self, string, começo):
-        if começo:
-            string = self.inOrder_print(começo.esquerda, string)
-            string += (str(começo.value) + "-")
-            string = self.inOrder_print(começo.direita, string)
-        return string
+        elif atual.esquerda == None:
+            if atual == self.root:
+                self.root = atual.direita
+            else:
+                if filho_esquerda:
+                    pai.esquerda = atual.direita
+                else:
+                    pai.direita = atual.direita
 
-    def postorder_print(self, string, começo):
+        else:
+            sucessor = self.nosucessor(atual)
+            if atual == self.root:
+                self.root = sucessor
+            else:
+                if filho_esquerda:
+                    pai.esquerda = sucessor
+                else:
+                    pai.direita = sucessor
+            sucessor.esquerda = atual.esquerda
 
-        if começo:
-            string = self.postorder_print(começo.esquerda, string)
-            string = self.postorder_print(começo.direita, string)
-            string += (str(começo.value) + "-")
-        return string
+        return True
 
-
-    def printArvore(self):
-        if self.root is not None:
-            self._printArvore(self.root)
-
-
-    def _printArvore(self, node):
+    def printArvore(self, node):
         if node is not None:
-            self._printArvore(node.esquerda)
+            self.printArvore(node.esquerda)
             print(str(node.valor) + ' ')
-            self._printArvore(node.direita)
-
-
+            self.printArvore(node.direita)
